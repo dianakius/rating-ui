9b1cbdebdeee08a9a7725443831fe5a76655082d
dianakius <eingidiana@gmail.com>
2025-10-06 17:52:53 +0300

    Implement changes from Rating to Modal component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Star from "./Star";
+import Modal from "./Modal";
 
 const Rating = ({
-  heading = 'Rate Your Experience', 
-  color= 'gold', 
+  heading = "Rate Your Experience",
+  color = "gold",
   feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"],
 }) => {
   const [rating, setRating] = useState(0);
@@ -11,56 +12,49 @@ const Rating = ({
   const [submitted, setSubmitted] = useState(false);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
-  
+
   const handleSubmit = () => {
     if (rating > 0) {
       setSubmitted(true);
     }
   };
 
-const closeModal = () => {
-  setSubmitted(false);
-  setRating(0);
-  setHover(0);
-}
+  //Close modal and reset UI
+  const closeModal = () => {
+    setSubmitted(false);
+    setRating(0);
+    setHover(0);
+  };
 
   return (
     <div className="rating-container">
-      <h2>{ heading}</h2>
+      <h2>{heading}</h2>
 
       <div className="stars">
         {stars.map((star) => (
-          <Star 
-          key={star} 
-          star={star}
-          rating={rating}
-          hover={hover}
-          color={color}
-          ratingClick= {setRating}
-          hoverEnter={setHover}
-          hoverLeave={() => setHover(null)}
+          <Star
+            key={star}
+            star={star}
+            rating={rating}
+            hover={hover}
+            color={color}
+            ratingClick={setRating}
+            hoverEnter={setHover}
+            hoverLeave={() => setHover(null)}
           />
         ))}
       </div>
-      {rating > 0 && 
+      {rating > 0 && (
         <p className="feedback"> {feedbackMessages[rating - 1]} </p>
-      }
-      <button className='submit-btn' onClick={handleSubmit} disabled={rating === 0}
-      >Submit</button>
-
-      {/*Modal */}
-      { submitted && (
-        <div className ="modal-overlay">
-          <div className ="modal">
-            <h2>Thank you </h2>
-            <p>You rated us { rating} star {rating > 1 ? 's' :''}</p>
-            <button className="close-btn" onClick={closeModal}>
-              Close
-            </button>
-            </div>
-            </div>
-          )
-          }
+      )}
+      <button
+        className="submit-btn"
+        onClick={handleSubmit}
+        disabled={rating === 0}
+      >
+        Submit
+      </button>
+      <Modal isOpen={ submitted } onClose={closeModal} rating ={rating}/>
     </div>
   );
 };
