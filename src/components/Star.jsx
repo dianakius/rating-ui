c09516e3b6ffc114013f9551b01590b56ab7013e
dianakius <eingidiana@gmail.com>
2025-10-06 17:17:54 +0300

    Adding event handlers to the element

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,5 +1,16 @@
-const Star = () => {
-    return <span>Star</span>;
+const Star = ({star , rating, hover, color, ratingClick, hoverEnter, hoverLeave}) => {
+    return (
+    <span 
+    onClick={() => ratingClick(star)}
+    onMouseEnter={() => hoverEnter(star)}
+    onMouseLeave={hoverLeave}
+    className='star'
+    style={{color: star <= (hover || rating) ? color
+        : '#ccc'}}
+    >
+        {'\u2605'}
+        </span>
+        );
 };
 
 export default Star;
